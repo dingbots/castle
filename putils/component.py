43261f42fde67ca4d2f29a11373fc138e8f2a82e
Jamie Bliss <jamie@ivyleav.es>
2019-07-06 12:31:14 -0400

    Rebuild @component to use a real class, so more advanced subclassing can be done

--- putils/component.py
+++ putils/component.py
@@ -9,6 +9,44 @@ import asyncio
 from .paio import FauxOutput, task, unwrap
 
 
+class Component(pulumi.ComponentResource):
+    def __init__(self, __name__, *pargs, __opts__=None, **kwargs):
+        super().__init__(self.__namespace__, __name__, None, __opts__)
+        futures = {}
+        # Build out the declared outputs so they're available immediately
+        for name in self.__outputs__:
+            futures[name] = asyncio.get_event_loop().create_future()
+            # FIXME: Use real Outputs instead of FauxOutputs
+            setattr(self, name, FauxOutput(futures[name]))
+
+        self._inittask(futures, __name__, *pargs, __opts__=__opts__, **kwargs)
+
+    @task
+    async def _inittask(self, futures, *pargs, **kwargs):
+        # Wraps up the initialization function and marshalls the data around
+        try:
+            # Call the initializer
+            outs = await unwrap(self.set_up(*pargs, **kwargs))
+        except Exception as e:
+            # Forward the exception to the futures, so they don't hang
+            for f in futures.values():
+                f.set_exception(e)
+            raise
+        else:
+            # Process the returned outputs
+            if outs is None:
+                outs = {}
+            self.register_outputs(outs)
+            for name, value in outs.items():
+                if name in futures:
+                    futures[name].set_result(value)
+                else:
+                    setattr(self, name, value)
+
+    def set_up(self, *pargs, **kwargs):
+        pass
+
+
 def component(namespace=None, outputs=()):
     """
     Makes the given callable a component, with much less boilerplate.
@@ -25,44 +63,13 @@ def component(namespace=None, outputs=()):
         if namespace is None:
             namespace = f"{func.__module__.replace('.', ':')}:{func.__name__}"
 
-        def __init__(self, __name__, *pargs, __opts__=None, **kwargs):
-            super(klass, self).__init__(namespace, __name__, None, __opts__)
-            futures = {}
-            # Build out the declared outputs so they're available immediately
-            for name in outputs:
-                futures[name] = asyncio.get_event_loop().create_future()
-                # FIXME: Use real Outputs instead of FauxOutputs
-                setattr(self, name, FauxOutput(futures[name]))
-
-            @task
-            async def inittask():
-                # Wraps up the initialization function and marshalls the data around
-                try:
-                    # Call the initializer
-                    outs = await unwrap(func(self, __name__, *pargs, __opts__=__opts__, **kwargs))
-                except Exception as e:
-                    # Forward the exception to the futures, so they don't hang
-                    for f in futures.values():
-                        f.set_exception(e)
-                    raise
-                else:
-                    # Process the returned outputs
-                    if outs is None:
-                        outs = {}
-                    self.register_outputs(outs)
-                    for name, value in outs.items():
-                        if name in futures:
-                            futures[name].set_result(value)
-                        else:
-                            setattr(self, name, value)
-
-            inittask()
-
-        klass = type(func.__name__, (pulumi.ComponentResource,), {
-            '__init__': __init__,
+        klass = type(func.__name__, (Component,), {
             '__doc__': func.__doc__,
             '__module__': func.__module__,
             '__qualname__': func.__qualname__,
+            'set_up': func,
+            '__namespace__': namespace,
+            '__outputs__': outputs,
         })
         return klass
 
