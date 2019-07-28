5ce500bd310c942d7b8579d10a6dafc86ef9ff9d
Jamie Bliss <jamie@ivyleav.es>
2019-07-27 23:26:19 -0400

    spam: Update for new awsgi version

--- spam/__main__.py
+++ spam/__main__.py
@@ -13,4 +13,21 @@ def index():
 
 
 def main(event, context):
-    return awsgi.response(app, event, context)
+    print("event", event)
+    rv = awsgi.response(app, event, context, base64_content_types={"image/png"})
+    print("result", rv)
+    return rv
+
+
+def test(event, context):
+    print("event", event)
+    print("context", context)
+    return {
+        "statusCode": 200,
+        "statusDescription": "200 OK",
+        "isBase64Encoded": False,
+        "headers": {
+            "Content-Type": "text/html"
+        },
+        "body": "<h1>Hello from Lambda!</h1>"
+    }
