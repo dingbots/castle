8e48fc20b5df7d62a2c2b285601d07ecb0f715b3
Jamie Bliss <jamie@ivyleav.es>
2019-08-06 13:41:35 -0400

    Move putils and deplumi into their own repos

--- putils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A collection of utilities to make Pulumi much more palatable.
-"""
-
-from .localstack import *  # noqa: F403 F401
-from .paio import *  # noqa: F403 F401
-from .component import *  # noqa: F403 F401
-from .aws import *  # noqa: F403 F401
