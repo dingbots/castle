36ab798f99ca004b8817272450abd11396e8b095
Jamie Bliss <jamie@ivyleav.es>
2019-08-23 17:45:36 -0400

    Move the site to its own repo

--- __main__.py
+++ __main__.py
@@ -1,21 +1,11 @@
 import pulumi
 from putils import opts, FauxOutput
-from staticsite import StaticSite
 from deplumi import Package, AwsgiHandler
 from pulumi_aws import route53, s3
 
 config = pulumi.Config('castle')
 
 zone = FauxOutput(route53.get_zone(name='dingbots.dev'))
-
-# Create an AWS resource (S3 Bucket)
-site = StaticSite(
-    'MainSite',
-    domain=config.require('domain'),
-    zone=zone,
-    content_dir='www',
-    **opts(),
-)
 
 buf = s3.Bucket(
     'MyBucket',
